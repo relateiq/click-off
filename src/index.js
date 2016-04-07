5f49fbe01e2e011392e5a1b34f725f9596534b90
Sterling Camden <sterling@relateiq.com>
2016-04-07 14:48:50 -0700

    copy click off logic from uiq

---
+++ src/index.js
@@ -0,0 +1,24 @@
+module.exports.listen = function listenForClickOff(elem, onClickOff, opts) {
+	// we use mousedown to check the target because the click could cause the element to be removed and it'll look like it's not in us
+	var lastMouseDownWasOutside = false;
+	// check again after timeout
+	var eventListenerElement = opts.eventListenerElement || document;
+	var isInMe = opts.isInMe;
+
+	function mousedownHandler(e) {
+		lastMouseDownWasOutside = !(elem === e.target || elem.contains(e.target)) && !(isInMe && isInMe(e.target));
+	}
+
+	function mouseupHandler(e) {
+		if (lastMouseDownWasOutside) {
+			onClickOff(e);
+		}
+	}
+	eventListenerElement.addEventListener('mousedown', mousedownHandler);
+	eventListenerElement.on('mouseup', mouseupHandler);
+
+	return function unbindClickOffListeners() {
+		eventListenerElement.removeEventListener(mousedownHandler);
+		eventListenerElement.removeEventListener(mouseupHandler);
+	};
+};
