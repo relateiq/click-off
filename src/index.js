9ffef65f9fc3c7e7dcd88d3f3aa8896e92d1a4f6
Sterling Camden <sterling@relateiq.com>
2016-04-08 12:10:00 -0700

    allow elem to be a getter function as well

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 module.exports.listen = function listenForClickOff(elem, onClickOff, opts) {
 	var parsedOpts = opts || {}
-	// we use mousedown to check the target because the click could cause the element to be removed and it'll look like it's not in us
+		// we use mousedown to check the target because the click could cause the element to be removed and it'll look like it's not in us
 	var lastMouseDownWasOutside = false;
 	// check again after timeout
 	var eventListenerElement = parsedOpts.eventListenerElement || document;
 	var isInMe = parsedOpts.isInMe;
 
 	function mousedownHandler(e) {
+		if (typeof elem === 'function') {
+			elem = elem();
+		}
 		lastMouseDownWasOutside = !(elem === e.target || elem.contains(e.target)) && !(isInMe && isInMe(e.target));
 	}
 
